9675ee1eb23fb11da7ae8038dfce92d496c35ecd
Gorloff <gorloff.slava@gmail.com>
2017-09-30 21:22:37 +0600

    2/5/1

--- BackToThePast/ViewController.swift
+++ BackToThePast/ViewController.swift
@@ -7,17 +7,32 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var audioPlayer: AVAudioPlayer!
+    let soundArray = ["C", "D", "E", "F", "G", "A", "B"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func notePressed(_ sender: UIButton) {
+        let soundURL = Bundle.main.url(forResource: "note_" + self.soundArray[sender.tag].lowercased(), withExtension: "wav")
+        do {
+            self.audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+        } catch {
+            print(error)
+        }
+        self.audioPlayer.play()
     }
 
 
